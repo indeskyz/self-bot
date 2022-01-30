5ea34235f20222ca1fb457da48759e2b826dee35
indeskyz <codishazey@yahoo.ca>
2022-01-30 00:14:40 -0500

    redo background task for bumping server

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 bot_token = os.getenv('BOT_TOKEN')
-server_id = os.getenv('SERVER_ID')
+channel_id = os.getenv('CHANNEL_ID')
 preifx = ""
 disboard_prefix = "!d"
 bump_msg = disboard_prefix + " bump"
--- index.py
+++ index.py
@@ -16,57 +16,57 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 
-# class MyClient(discord.Client):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
+class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-#         # an attribute we can access from our task
-#         self.bump_command = config.bump_msg
+        # an attribute we can access from our task
+        self.bump_command = config.bump_msg
 
-#         # start the task to run in the background
-#         self.my_background_task.start()
+        # start the task to run in the background
+        self.my_background_task.start()
 
-#     async def on_ready(self):
-#         print('Logged in as')
-#         print(self.user.name)
-#         print(self.user.id)
-#         print('------')
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
 
-#     @tasks.loop(seconds=config.wait_time)
-#     async def my_background_task(self):
-#         channel = self.get_channel(config.server_id) 
-#         await channel.send(self.bump_command)
+    @tasks.loop(seconds=5)
+    async def my_background_task(self):
+        channel = self.get_channel(int(config.channel_id)) 
+        await channel.send(self.bump_command)
 
-#     @my_background_task.before_loop
-#     async def before_my_task(self):
-#         await self.wait_until_ready()
+    @my_background_task.before_loop
+    async def before_my_task(self):
+        await self.wait_until_ready()
 
-# client = MyClient()
-# client.run(config.bot_token)
+client = MyClient()
+client.run(config.bot_token, bot=False)
 
 
-bot = commands.Bot(config.preifx, self_bot=True)
+# bot = commands.Bot(config.preifx, self_bot=True)
 
-@bot.event
-async def on_ready():
-    if config.stream:
-        await bot.change_presence(activity=discord.Streaming(name="Marc Jacobs Spring 1998", url="https://www.youtube.com/watch?v=Nv33ni_acFs&list=PLVo48CvBKNtjZiFwKNX0bRhn4wrISpP7c&index=2&t=6s"))
-    print('logged in')
+# @bot.event
+# async def on_ready():
+#     if config.stream:
+#         await bot.change_presence(activity=discord.Streaming(name="Marc Jacobs Spring 1998", url="https://www.youtube.com/watch?v=Nv33ni_acFs&list=PLVo48CvBKNtjZiFwKNX0bRhn4wrISpP7c&index=2&t=6s"))
+#     print('logged in')
     
 
-@bot.command()
-async def bump(ctx):
+# @bot.command()
+# async def bump(ctx):
     
-    async def sendMsg(msg):
-        async with ctx.typing():
-            await asyncio.sleep(randint(2, 9))
-        await ctx.send(msg)
+#     async def sendMsg(msg):
+#         async with ctx.typing():
+#             await asyncio.sleep(randint(2, 9))
+#         await ctx.send(msg)
     
-    while True:
-        await sendMsg(config.bump_msg)
-        await asyncio.sleep(config.wait_time)
+#     while True:
+#         await sendMsg(config.bump_msg)
+#         await asyncio.sleep(config.wait_time)
 
 
 
-bot.run(config.bot_token, bot=False)
+# bot.run(config.bot_token, bot=False)
 
